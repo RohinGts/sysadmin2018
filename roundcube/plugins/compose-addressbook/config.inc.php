06891892ad4b7c824a2837477967f3a0b04b0394
RohinGts <root@sambath-kumar-vm.rohin.est>
2018-05-17 02:11:31 +0300

    Exam files

---
+++ roundcube/plugins/compose-addressbook/config.inc.php
@@ -0,0 +1,3 @@
+<?php
+// Empty configuration for compose-addressbook
+// See /usr/share/roundcube/plugins/compose-addressbook/config.inc.php.dist for instructions
---
+++ roundcube/plugins/dovecot-impersonate/config.inc.php
@@ -0,0 +1,3 @@
+<?php
+// Empty configuration for dovecot-impersonate
+// See /usr/share/roundcube/plugins/dovecot-impersonate/config.inc.php.dist for instructions
